ed1d4b5fdefe4c50955eecc58f7af74c7cc1e990
sowmyaneni <ssneni@gmail.com>
2019-05-01 19:55:11 -0700

    Work towards building a chat database.

--- server/schema.sql
+++ server/schema.sql
@@ -4,12 +4,29 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  `id` INTEGER AUTO_INCREMENT NOT NULL,
+  `text` MEDIUMTEXT DEFAULT NULL,
+  `id_username` INTEGER NULL DEFAULT NULL,
+  `id_room` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
 /* Create other tables and define schemas for them here! */
+DROP TABLE IF EXISTS `username`;
+		
+CREATE TABLE `username` (
+  `id` INTEGER AUTO_INCREMENT NOT NULL,
+  `name` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 
-
-
+DROP TABLE IF EXISTS `room`;
+		
+CREATE TABLE `room` (
+  `id` INTEGER AUTO_INCREMENT NOT NULL,
+  `name` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
